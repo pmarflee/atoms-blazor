e895b14535f0955ff0a64a64930c060a922d6a1b
Paul Marfleet <paul.marfleet@gmail.com>
2024-11-21 23:27:50 +0000

    Display using alternate colour scheme & atom shapes when options are selected

--- src/Atoms.Web/Components/Pages/Home.razor.cs
+++ src/Atoms.Web/Components/Pages/Home.razor.cs
@@ -1,9 +1,12 @@
-﻿using Atoms.Core.Enums;
+﻿using Microsoft.JSInterop;
 
 namespace Atoms.Web.Components.Pages;
 
 public partial class HomePageComponent : ComponentBase
 {
+    [Inject]
+    IJSRuntime JSRuntime { get; set; } = default!;
+
     protected GameState State { get; set; }
     protected Game? Game { get; set; }
 
@@ -14,12 +17,28 @@ public partial class HomePageComponent : ComponentBase
         base.OnInitialized();
     }
 
-    protected Task OnCreateGame(Game game)
+    protected async Task OnCreateGame(Game game)
     {
         Game = game;
         State = GameState.Game;
 
-        return Task.CompletedTask;
+        if (Game.ColourScheme == ColourScheme.Alternate)
+        {
+            await JSRuntime.InvokeVoidAsync("App.setAlternateColourScheme");
+        }
+        else
+        {
+            await JSRuntime.InvokeVoidAsync("App.setDefaultColourScheme");
+        }
+
+        if (Game.AtomShape == AtomShape.Varied)
+        {
+            await JSRuntime.InvokeVoidAsync("App.setVariedAtomShape");
+        }
+        else
+        {
+            await JSRuntime.InvokeVoidAsync("App.setDefaultAtomShape");
+        }
     }
 
     protected Task OnShowMenu()
---
+++ src/Atoms.Web/wwwroot/js/app.js
@@ -0,0 +1,14 @@
+﻿window.App = class {
+    static setDefaultColourScheme() {
+        document.documentElement.classList.remove('high-contrast');
+    }
+    static setAlternateColourScheme() {
+        document.documentElement.classList.add('high-contrast');
+    }
+    static setDefaultAtomShape() {
+        document.documentElement.classList.remove('shaped-atoms');
+    }
+    static setVariedAtomShape() {
+        document.documentElement.classList.add('shaped-atoms');
+    }
+}
