a3109182b07503678365c082ed2cf04684dc885c
Paul Marfleet <paul.marfleet@gmail.com>
2025-01-01 17:37:15 +0000

    Test playing randomly

---
+++ test/Atoms.UnitTests/Core/AI/Strategies/PlayRandomlyTests.cs
@@ -0,0 +1,49 @@
+﻿namespace Atoms.UnitTests.Core.AI.Strategies;
+
+public class PlayRandomlyTests : AIStrategyTestFixture
+{
+    [Test]
+    public async Task ShouldChooseACellRandomly()
+    {
+        var game = ObjectMother.Game();
+        var cell = game.Board[1, 1];
+        var strategy = Strategy(1, 1);
+
+        await Assert.That(strategy.Choose(game)).IsEqualTo(cell);
+    }
+
+    [Test]
+    public async Task ShouldNotChooseACellOccupiedByAnotherPlayer()
+    {
+        var game = ObjectMother.Game(
+            active: 2,
+            cells: [ new(1, 1, 1, 1) ]);
+
+        var strategy = Strategy(1, 1, 2, 2);
+
+        await Assert.That(strategy.Choose(game)).IsEqualTo(game.Board[2, 2]);
+    }
+
+    static PlayRandomly Strategy(params List<int> values) => new(new PlayRandomlyNumberGenerator(values));
+
+    class PlayRandomlyNumberGenerator(params List<int> values) 
+        : IRandomNumberGenerator
+    {
+        private int _index = 0;
+
+        public int Next(int maxValue)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Next(int minValue, int maxValue)
+        {
+            return values[_index++];
+        }
+
+        public double NextDouble()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
